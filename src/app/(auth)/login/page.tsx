97d76e521fcfb6a2c72e2d9f81338620db1acfa6
gauravkharel <kharel619@gmail.com>
2023-09-12 14:34:34 +0545

    ssc to fetch data

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,8 +1,10 @@
-import { FC } from 'react'
+// rendering server side component getting data
 
+import axios from "axios"
+const login = async() => {
+  const {data} = await axios.get('https://jsonplaceholder.typicode.com/posts/1')
 
-const login = ({}) => {
-  return <div>pages</div>
+  return <div>{JSON.stringify(data)}</div>
 }
 
 export default login
