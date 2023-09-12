3c65029f57c8a25341a5f9d6abe3636cc0edfa0b
gauravkharel <kharel619@gmail.com>
2023-09-12 14:22:53 +0545

    auth route

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'react'
+
+
+const login = ({}) => {
+  return <div>pages</div>
+}
+
+export default login
--- src/app/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from 'react'
-
-
-
-const page = ({}) => {
-  return <div>page</div>
-}
-
-export default page
